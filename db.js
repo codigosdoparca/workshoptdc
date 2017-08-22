905f23e7dfb09a239fb0fa0002f7e0b7fc6f8325
codigosdoparca <codigosdoparca@gamail.com>
2017-08-22 00:14:06 -0400

    Commitando projeto - CRUD - node + mongoDB

---
+++ db.js
@@ -0,0 +1,28 @@
+var mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb://localhost/workshoptdc")
+            .then(conn => global.conn = conn)
+            .catch(err => console.log(err))
+
+function findAll(callback){  
+    global.conn.collection("customers").find({}).toArray(callback);
+}
+
+var ObjectId = require("mongodb").ObjectId;
+function findOne(id, callback){  
+    global.conn.collection("customers").find(new ObjectId(id)).toArray(callback);
+}
+
+function insert(customer, callback){
+    global.conn.collection("customers").insert(customer, callback);
+}
+
+function update(id, customer, callback){
+    // global.conn.collection("customers").updateOne(new ObjectId(id), customer, callback);
+       global.conn.collection("customers").update({_id: id }, customer, callback);
+}
+
+function deleteOne(id, callback){
+    global.conn.collection("customers").deleteOne({_id: new ObjectId(id)}, callback);
+}
+
+module.exports = { findAll, insert, findOne, update, deleteOne };
